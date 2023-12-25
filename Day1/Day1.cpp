6969759086b71b480d4df3001ae53bbebad07369
Juqur <qureshijunaid2001@gmail.com>
2023-12-25 12:49:42 -0500

    Completed Day 1 part 2

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+struct NumberInfo {
+    int digit;  
+    int position;  
+};
+
+string numberWords[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+string line;
+int sum = 0;
+ifstream file("Data.txt");
+
 int main(){
-    string line;
-    int sum = 0;
-    ifstream file("Data.txt");
     while (std::getline(file, line)) {
-        int first;
-        int last;
+        std::vector<NumberInfo> numbers;
+        int i = 0;
         for (auto it = line.begin(); it != line.end(); ++it) {
             if (isdigit(*it)) {
-                first = *it - '0';
-                break; 
+                NumberInfo info = {*it - '0', i};
+                numbers.push_back(info);
+            }
+            i++;
+        }
+        for (int i = 0; i <= 9; ++i) {
+            int pos = line.find(numberWords[i]);
+            while (pos != -1) {
+                NumberInfo info = {i, pos};
+                numbers.push_back(info);
+                pos = line.find(numberWords[i], pos + 1);
             }
         }
-        for (auto it = line.rbegin(); it != line.rend(); ++it) {
-            if (isdigit(*it)) {
-                last = *it - '0';
-                break; 
-            }
-        }
-        sum += first*10 + last;
+        sort(numbers.begin(), numbers.end(), [](const NumberInfo& a, const NumberInfo& b) {
+            return a.position < b.position;
+        });
+        sum += (numbers.front().digit ) * 10 + (numbers.back().digit);
     }
     file.close();
-    cout << sum;
+    std::cout << sum;
     return 1;
 }
